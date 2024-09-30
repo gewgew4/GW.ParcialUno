caf7c80082765c360de2525175ebd476f8117c22
gewgew4 <29262845+gewgew4@users.noreply.github.com>
2024-09-30 03:25:04 -0300

    Add new properties to PrintJobMessage and new PrintStatusMessage
    Two new properties, `DocumentName` and `CreatedAt`, were added to the
    `PrintJobMessage` class. `DocumentName` is a string representing the
    name of the document, and `CreatedAt` is a DateTime indicating when
    the print job was created.
    
    A new class, `PrintStatusMessage`, was added in the `Common.Messages`
    namespace. This class includes three properties: `OK` (a string with
    a default value of "OK"), `PrintDate` (a DateTime with a default value
    of `DateTime.MinValue`), and `DocumentName` (a string with a default
    value of an empty string).

--- Common/Messages/PrintJobMessage.cs
+++ Common/Messages/PrintJobMessage.cs
@@ -5,5 +5,7 @@ public class PrintJobMessage
     public Guid JobId { get; set; }
     public byte[] Content { get; set; }
     public Guid DocumentId { get; set; }
+    public string DocumentName { get; set; }
     public byte Priority { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
---
+++ Common/Messages/PrintStatusMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Messages;
+
+public class PrintStatusMessage
+{
+    public string OK { get; set; } = "OK";
+    public DateTime PrintDate { get; set; } = DateTime.MinValue;
+    public string DocumentName { get; set; } = string.Empty;
+}
